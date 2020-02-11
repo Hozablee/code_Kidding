99c64aa868fccf73d381060e437cd8d1b107790a
Hozablee <49124734+Hozablee@users.noreply.github.com>
2020-02-11 20:10:40 +0700

    Add files via upload

---
+++ factorrrrrs.py
@@ -0,0 +1,10 @@
+def L_prime(num):
+    div=2
+    while num >= 1:
+        while num%div==0:
+            if num%div == 0:
+                print(div)
+                num=num//div
+        div=div+1
+    
+            
