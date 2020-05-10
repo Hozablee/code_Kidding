81fe5097670dc1b67d1ea3ff840b0f273356416f
gaivits <49124734+gaivits@users.noreply.github.com>
2020-05-10 15:21:39 +0700

    Add files via upload

--- stringMat.cpp
+++ stringMat.cpp
@@ -1,34 +1,24 @@
 #include<iostream>
-#include<string.h>
+#include<string>
 using namespace std;
-bool finds(string T,string P)
+bool Finds(string str,string p)
 {
-    int j = 0;
-    int n = 0;
-    int m = P.size()-1;
-    for(int i = 0;i<m-n;i++)
+    int init = 0;
+    int lasts = str.size()-1;
+    int checks = 0;
+    for(int i = 0;i<=str.size()-init;i++)
     {
-        while(j<=P.size() && P[n]==T[n+m])
+        while(checks <= str.size() && p[checks] == str[checks+i])
         {
-            j=j+1;
-            if(j==m)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-            m--;
+            checks++;
+            return true;
         }
+        return false;
     }
 }
 int main()
 {
-    string a;
-    cin >> a;
-    string b;
-    cin >> b;
-    cout << finds(a,b);
-
+    string a,b;
+    cin >> a >> b;
+    cout << Finds(a,b);
 }
