b59322936edb7358df84c60834c26bf5bf3b7653
gaivits <49124734+gaivits@users.noreply.github.com>
2020-05-29 19:47:06 +0700

    Add files via upload

---
+++ tripluszero.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int z=0;z<n;z++)
+    {
+        cin >> arr[z];
+    }
+    for(int i = 0;i<n;i++)
+    {
+        for(int j=0;j<n-1;j++)
+        {
+            for(int k=0;k<n-2;k++)
+            {
+               if(arr[i]+arr[j]+arr[k]==0)
+                {
+                    cout << arr[i] <<" " << arr[j] << " " << arr[k] << endl;
+                }
+                k++;
+            }
+            j++;
+        }
+
+    }
+}
