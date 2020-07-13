616e5ba660d373a9392f70e79b7de063058bb821
gaivits <49124734+gaivits@users.noreply.github.com>
2020-07-13 22:11:35 +0700

    Add files via upload

---
+++ Almost-o1.py
@@ -0,0 +1,25 @@
+def jump(N):
+    ans=[]
+    ans.append(1)
+    ans.append(1)
+    ans.append(1)
+    ans.append(2)
+    ans.append(4)
+    for i in range(5,N):
+      a=ans[0]+ans[1]+ans[4]
+      
+      ans[0] = ans[1]
+      ans[1] = ans[2]
+      ans[2] = ans[3]
+      ans[3] = ans[4]
+      ans[4] = a
+    return(ans[4])
+if _name_ == "_main_":
+  N = int(input())
+  if N < 4:
+    print(1)
+  elif N == 4:
+    print(2)
+  else:
+    a = jump(N)
+    print(a)
