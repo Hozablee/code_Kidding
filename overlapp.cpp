a8d218364cd31acaa3140d7d4067ba607d4a10b3
gaivits <49124734+gaivits@users.noreply.github.com>
2021-02-28 20:25:26 +0700

    Add files via upload

--- overlapp.cpp
+++ overlapp.cpp
@@ -1,33 +1,30 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-#include<stdlib.h>
 using namespace std;
 int main()
 {
-    int c = 0;
-    vector< pair<int,int> > v1;
-    int n ;
-    int num1,num2;
-    cin >> n;
-    for(int i = 0;i<n;i++)
+    vector< pair<int,int> > vec;
+    int sz;
+    int x,y;
+    cin >> sz;
+    int cnt = 0;
+    for(int j = 0;j<sz;j++)
     {
-        cin >> num1 >> num2;
-        num1 = num1%104729;
-        num2 = num2%104729;
-        v1.push_back( pair<int,int> (num1,num2) );
-        sort(v1.begin(),v1.end());
+        cin >> x >> y;
+        vec.push_back( pair<int,int> (x,y) );
     }
-    for(int x = 0;x<n;x++)
+    sort(vec.begin(),vec.end());
+    for(int a=0;a<sz;a++)
     {
-        //cout << v1[x].first << " " << v1[x].second << endl;
-       for(int y = v1[x].first;y<v1[x].second;y++)
+        for(int b = 0;b<vec[a].first;b++)
         {
-           if(y > v1[x+1].first && y <= v1[x+1].second)
-           {
-               c++;
-           }
+            if(vec[a].first >= vec[b].second)
+            {
+                cnt++;
+            }
         }
     }
-    cout << c;
+    cnt=cnt+1;
+    cout << cnt;
 }
